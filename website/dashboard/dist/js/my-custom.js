d58cff761a0706492e8098a993c07277dc029ac7
bayuagung100 <bayuagung100@gmail.com>
2020-01-26 21:03:41 +0700

    sampai preview pertyaan 1

--- website/dashboard/dist/js/my-custom.js
+++ website/dashboard/dist/js/my-custom.js
@@ -13,3 +13,97 @@ function readImg(input, id) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+$(document).ready(function(){
+    // var form = document.getElementById('buat_quiz');
+    var judul = document.forms["buat_quiz"]["judul"];               
+    var gambar = document.forms["buat_quiz"]["gambar"];    
+    var kategori = document.forms["buat_quiz"]["kategori"];  
+    var tingkat =  document.forms["buat_quiz"]["tingkat"];  
+    var deskripsi = document.forms["buat_quiz"]["deskripsi"];
+    $("#next_pertanyaan1").click(function(){
+        // if(judul.value == "" && judul.hasAttribute('required')){
+        //     alert("Nama quiz wajib diisi ");
+        //     judul.focus();
+        //     return false; 
+        // }
+
+        // if(gambar.value == "" && gambar.hasAttribute('required')){
+        //     alert("Gambar quiz wajib diisi ");
+        //     gambar.focus();
+        //     return false; 
+        // }
+
+        // if(kategori.value == "" && kategori.hasAttribute('required')){
+        //     alert("Kategori quiz wajib diisi ");
+        //     kategori.focus();
+        //     return false; 
+        // }
+
+        // if(tingkat.value == "" && tingkat.hasAttribute('required')){
+        //     alert("Tingkat kesulitan quiz wajib diisi ");
+        //     tingkat.focus();
+        //     return false; 
+        // }
+
+        // if(deskripsi.value == "" && deskripsi.hasAttribute('required')){
+        //     alert("Deskripsi quiz wajib diisi ");
+        //     deskripsi.focus();
+        //     return false; 
+        // } 
+
+        $('#banner').hide();
+        $('#tanda').hide();
+        $('#default').hide();
+        $('#pertanyaan1').show();
+    });
+
+    $("#prev_default").click(function(){
+        $('#banner').show();
+        $('#tanda').show();
+        $('#default').show();
+        $('#pertanyaan1').hide();
+    });
+
+    $("#a_soal1").click(function(){
+        $("#jawaban_a_soal1").prop('required', true);
+        $("#jawaban_b_soal1").prop('required', false);
+        $("#jawaban_c_soal1").prop('required', false);
+        $("#jawaban_d_soal1").prop('required', false);
+    });
+
+    $("#b_soal1").click(function(){
+        $("#jawaban_a_soal1").prop('required', false);
+        $("#jawaban_b_soal1").prop('required', true);
+        $("#jawaban_c_soal1").prop('required', false);
+        $("#jawaban_d_soal1").prop('required', false);
+    });
+
+    $("#c_soal1").click(function(){
+        $("#jawaban_a_soal1").prop('required', false);
+        $("#jawaban_b_soal1").prop('required', false);
+        $("#jawaban_c_soal1").prop('required', true);
+        $("#jawaban_d_soal1").prop('required', false);
+    });
+
+    $("#d_soal1").click(function(){
+        $("#jawaban_a_soal1").prop('required', false);
+        $("#jawaban_b_soal1").prop('required', false);
+        $("#jawaban_c_soal1").prop('required', false);
+        $("#jawaban_d_soal1").prop('required', true);
+    });
+
+
+});
+
+$(document).ready(function(){
+    var soal1 = $('#soal1').keyup(function(){
+        isi = this.value;
+        $('#preview-container-soal1').text(isi);
+    });
+    
+    // var gambar = $('#preview2').click(function(){
+    //     console.log(gambar.load());
+    // });
+});
+
--- website/dashboard/pages/buat-quiz.php
+++ website/dashboard/pages/buat-quiz.php
@@ -1,40 +1,197 @@
 <section class="content" style="padding:20px">
     <div class="container-fluid">
         <div class="buat-quiz">
-            <div class="banner">
+            <div id="banner" class="banner">
                 <div class="image-wrapper">
                     <img src="<?php echo url("img/icons/createaquiz.png");?>">
                 </div>
                 <div class="title">Buat quiz</div>
             </div>
-            <form>
-                <p>
-                <label>1. Berikan nama quiz ini (Wajib diisi)</label>
-                <input type="text" name="judul" placeholder="Nama Quiz" required>
-                </p>
-                <p>
-                <label>2. Berikan gambar pada quiz ini (Wajib diisi)</label>
-                <img id="preview" src="#" alt="your image"/>
-                <input type="file" name="gambar" onchange="readImg(this, 'preview');" required>
-                <span style="float: left;">Format yang didukung: .jpeg, .jpg, .png</span>
-                <br>
-                </p>
-                <p>
-                <label>3. Berikan kategori untuk quiz ini (Wajib diisi)</label>
-                <div class="action-btns-wrapper">
-                    <button class="cancel-btn" onclick="history.back()">Batal</button>
-                    <button class="save-btn"><span class="">Selanjutnya</span></button>
+            <div id="tanda" class="container">
+                <p>Tanda (<span style="color:red">*</span>) wajib diisi.</p>
+            </div>
+            <form id="buat_quiz" >
+                <div id="default">
+                    <div class="form-group">
+                        <label for="judul">1. Berikan nama quiz ini <span style="color:red">*</span></label>
+                        <input type="text" id="judul" name="judul" placeholder="Nama Quiz" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="gambar">2. Berikan gambar pada quiz ini <span style="color:red">*</span></label>
+                        <br>
+                        <img id="preview" style="display:none" src="#" alt="your image" />
+                        <input style="margin-top:10px" type="file" id="gambar" name="gambar" onchange="readImg(this, 'preview');" required>
+                        <span style="float: left;">Format yang didukung: .jpeg, .jpg, .png</span>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="kategori">3. Berikan kategori untuk quiz ini <span style="color:red">*</span></label>
+                        <select id="kategori" name="kategori" class="form-control select2" style="width: 100%;" required>
+                            <option value="">Pilih Kategori</option>
+                            <option value="Pendidikan Agama">Pendidikan Agama</option>
+                            <option value="Pendidikan Kewarganegaraan">Pendidikan Kewarganegaraan</option>
+                            <option value="Bahasa Indonesia">Bahasa Indonesia</option>
+                            <option value="Bahasa Inggris">Bahasa Inggris</option>
+                            <option value="Matematika">Matematika</option>
+                            <option value="Fisika">Fisika</option>
+                            <option value="Kimia">Kimia</option>
+                            <option value="Biologi">Biologi</option>
+                            <option value="Sejarah">Sejarah</option>
+                            <option value="Seni Budaya">Seni Budaya</option>
+                            <option value="Penjaskes">Penjaskes</option>
+                            <option value="Teknologi Informasi dan Komunikasi">Teknologi Informasi dan Komunikasi</option>
+                            <option value="Keterampilan/Bahasa Asing">Keterampilan/Bahasa Asing</option>
+                            <option value="Muatan Lokal">Muatan Lokal</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="tingkat">4. Berikan tingkat kesulitan quiz ini <span style="color:red">*</span></label>
+                        <select id="tingkat" name="tingkat" class="form-control select2" style="width: 100%;" required>
+                            <option value="">Pilih Tingkat Kesulitan</option>
+                            <option value="Hard">Hard</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Easy">Easy</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="deskripsi">5. Deskripsi untuk quiz ini <span style="color:red">*</span></label>
+                        <textarea id="deskripsi" name="deskripsi" placeholder="Deskripsi Quiz" required></textarea>
+                    </div>
+                    <hr>
+                    <p>Catatan: Setiap quiz berisi 10 soal dengan total nilai 100</p>
+                    <div class="action-btns-wrapper">
+                        <button type="button" class="cancel-btn" onclick="history.back()">Batal</button>
+                        <button type="button" id="next_pertanyaan1" name="next_pertanyaan1" class="save-btn"><span>Selanjutnya</span></button>
+                    </div>
                 </div>
-                </p>
-                <p>
-                <label>4. Deskripsi untuk quiz ini (Wajib diisi)</label>
-                <textarea name="deskripsi" placeholder="Deskripsi Quiz" required></textarea>
-                </p>
-                <hr>
-                <p>Catatan: Setiap quiz berisi 10 soal dengan total nilai 100</p>
-                <div class="action-btns-wrapper">
-                    <button class="cancel-btn" onclick="history.back()">Batal</button>
-                    <button class="save-btn"><span class="">Selanjutnya</span></button>
+                <div id="pertanyaan1">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <div class="card card-default">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        <img style="width:25px" src="<?php echo url("img/icons/conversation.png");?>">
+                                        Pertanyaan 1
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="soal1">Pertanyaan<span style="color:red">*</span></label>
+                                        <textarea id="soal1" name="soal1" placeholder="Soal pertanyaan" required></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="gambar_soal1">Gambar pertanyaan (opsional)</label>
+                                        <br>
+                                        <img id="preview2" style="display:none" src="#" alt="your image" />
+                                        <input style="margin-top:10px" type="file" id="gambar_soal1" name="gambar_soal1" onchange="readImg(this, 'preview2');readImg(this, 'preview-container-gambar-soal1');">
+                                        <span style="float: left;">Format yang didukung: .jpeg, .jpg, .png</span>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label>Ceklis jawaban yang benar <i class="fa fa-check-circle" style="color: #2FDA40"></i> <span style="color:red">*</span></label>
+                                        <div class="form-group">
+                                            <div class="jawaban-container">
+                                                <input type="radio" id="a_soal1" name="jawaban_soal1" value="A" class="jawaban-radio">
+                                                <label for="a_soal1" class="jawaban-radio-label"></label>
+                                                <input class="jawaban-radio-input" type="text" id="jawaban_a_soal1" name="jawaban_a_soal1" placeholder="Pilihan jawaban 1">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="jawaban-container">
+                                                <input type="radio" id="b_soal1" name="jawaban_soal1" value="B" class="jawaban-radio">
+                                                <label for="b_soal1" class="jawaban-radio-label"></label>
+                                                <input class="jawaban-radio-input" type="text" id="jawaban_b_soal1" name="jawaban_b_soal1" placeholder="Pilihan jawaban 2">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="jawaban-container">
+                                                <input type="radio" id="c_soal1" name="jawaban_soal1" value="C" class="jawaban-radio">
+                                                <label for="c_soal1" class="jawaban-radio-label"></label>
+                                                <input class="jawaban-radio-input" type="text" id="jawaban_c_soal1" name="jawaban_c_soal1" placeholder="Pilihan jawaban 3">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="jawaban-container">
+                                                <input type="radio" id="d_soal1" name="jawaban_soal1" value="D" class="jawaban-radio">
+                                                <label for="d_soal1" class="jawaban-radio-label"></label>
+                                                <input class="jawaban-radio-input" type="text" id="jawaban_d_soal1" name="jawaban_d_soal1" placeholder="Pilihan jawaban 4">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="sticky-top mb-3">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4 class="card-title"><i class="fa fa-eye"></i> Preview pertanyaan 1</h4>
+                                        <!-- <div class="card-tools">
+                                            <button id="lihat-preview" type="button" class="btn btn-tool">
+                                                <i class="fa">&#xf021;</i> Refresh
+                                            </button>
+                                        </div> -->
+                                    </div>
+                                    <div id="preview-container" class="card-body" style="background: #390F3D;overflow:scroll;height:450px">
+                                        <div class="preview-soal text-center">
+                                            <!-- <img src="<?php echo url("img/icons/createaquiz.png");?>"/> -->
+                                            <img id="preview-container-gambar-soal1"  style="display:none" src="#" alt="your image" />
+                                            <br>
+                                            <h3 id="preview-container-soal1" ></h3>
+                                        </div>
+                                        <br>
+                                        <div class="card bg-gradient-danger">
+                                            <div class="card-header border-0 ui-sortable-handle">
+                                                <h3 class="card-title text-center" style="border: 2px solid #FFFFFF;border-radius: 10px;width:30px">
+                                                A 
+                                                </h3>
+                                                <i class="fa fa-check-circle" style="margin-left:10px;color: #2FDA40"></i>
+                                            </div>
+                                            <div class="card-body">
+                                                pilihan jawaban 1
+                                            </div>
+                                        </div>
+                                        <div class="card bg-gradient-warning">
+                                            <div class="card-header border-0 ui-sortable-handle">
+                                                <h3 class="card-title text-center" style="border: 2px solid #000;border-radius: 10px;width:30px">
+                                                B
+                                                </h3>
+                                            </div>
+                                            <div class="card-body">
+                                                pilihan jawaban 1
+                                            </div>
+                                        </div>
+                                        <div class="card bg-gradient-success">
+                                            <div class="card-header border-0 ui-sortable-handle">
+                                                <h3 class="card-title text-center" style="border: 2px solid #FFFFFF;border-radius: 10px;width:30px">
+                                                C
+                                                </h3>
+                                            </div>
+                                            <div class="card-body">
+                                                pilihan jawaban 1
+                                            </div>
+                                        </div>
+                                        <div class="card bg-gradient-primary">
+                                            <div class="card-header border-0 ui-sortable-handle">
+                                                <h3 class="card-title text-center" style="border: 2px solid #FFFFFF;border-radius: 10px;width:30px">
+                                                D
+                                                </h3>
+                                            </div>
+                                            <div class="card-body">
+                                                pilihan jawaban 1
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="action-btns-wrapper">
+                        <button type="button" id="prev_default" class="cancel-btn">Kembali</button>
+                        <button type="button" id="next_pertanyaan" class="save-btn"><span>Selanjutnya</span></button>
+                    </div>
                 </div>
             </form>
         </div>
